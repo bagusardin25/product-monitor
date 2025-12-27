272ad9e914258c2437604c2cf5735b167125789f
Bagus Ardin Prayoga <bagusardinp@gmail.com>
2025-12-27 09:05:45 +0700

    Implementasi Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('product');
+    return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('product');
-Route::get('/create', [ProductController::class, 'create'])->name('tambah');
-Route::post('/store', [ProductController::class, 'store'])->name('store');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('show');
-Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('edit');
-Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('update');
-Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
